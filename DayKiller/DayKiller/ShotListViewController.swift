7bd1b8fc0fbcd6f7d8fe9debaa5f8c05c5dcd76f
Павел Оливейра <lupideo@gmail.com>
2022-10-12 17:23:10 -0300

    setted  cell’s content and appearance and created and initialized the data source

--- DayKiller/DayKiller/ShotListViewController.swift
+++ DayKiller/DayKiller/ShotListViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ShotListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource = DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellResgistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let shot = Shot.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = shot.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellResgistration, for: indexPath, item: itemIdentifier)
+        })
     }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
